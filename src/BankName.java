d33eef183b403ac4a6b7691546278103aa2419d3
sasankdevarakonda <149362760+sasankdevarakonda@users.noreply.github.com>
2023-11-21 15:57:02 +0530

    show bank accounts

---
+++ src/BankName.java
@@ -0,0 +1,22 @@
+public class BankName {
+    public static void main(String[] args) {
+
+            String userChoice = "ICICI";
+            String selectedBank = getSelectedBank(userChoice);
+            System.out.println("Selected bank: " + selectedBank);
+        }
+        private static String getSelectedBank(String choice) {
+            if ("SBI".equals(choice)) {
+                return "SBI";
+            } else if ("HDFC".equals(choice)) {
+                return "HDFC";
+            } else if ("ICICI".equals(choice)) {
+                return "ICICI";
+            } else if ("RBI".equals(choice)) {
+                return "RBI";
+            } else {
+                return "Invalid choice";
+            }
+        }
+
+}
